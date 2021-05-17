023e1ac457846b7dc9f664cf00a69f1906043d5b
Rinkle Agarwal <64031090+rinklea@users.noreply.github.com>
2021-05-17 13:02:04 +0530

    Update delete-all-intercom-articles.py

--- delete-all-intercom-articles.py
+++ delete-all-intercom-articles.py
@@ -1,10 +1,10 @@
 import requests
 import json
 
-// intercom supports deletion of upto 30 articles a time so a loop will help to not the run the code multiple times
+#intercom supports deletion of upto 30 articles a time so a loop will help to not the run the code multiple times
 for articles in range(5):
     
-    // intercom url from which you have to fetch articles
+    #intercom url from which you have to fetch articles
     url_to_fetch='https://api.intercom.io/articles'
     
     headers = {
@@ -13,22 +13,22 @@ for articles in range(5):
         'Content-Type': 'application/json',
     }
     
-    //response will store all the retreived articles in JSON format
+    #response will store all the retreived articles in JSON format
     response = requests.get(url_to_fetch, headers=headers)
     
-    //to convert JSON file into a dictionary
+    #to convert JSON file into a dictionary
     articles_in_a_dictionary=json.loads(response.text)
    
     for each_article in articles_in_a_dictionary["data"]:
         
-            // intercom deletes article based on their id
+           # intercom deletes article based on their id
             id=each_article["id"]
             url_to_delete="https://api.intercom.io/articles/" + str(id)
             
-            //deleting article from the url
+            #deleting article from the url
             delete_response = requests.delete(url_to_delete, headers=headers)
     
-    // Should print 200
+    # Should print 200
     print(delete_response)
 
 
